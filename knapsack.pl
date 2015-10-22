7dc8539afcfb661bc739ca1be24125d7b9936bf4
Carolina <carolbramalho@gmail.com>
2015-10-22 02:26:12 -0200

    Comentários de funções

--- knapsack.pl
+++ knapsack.pl
@@ -14,22 +14,27 @@ knapsack :- % item(nomeMissao, tempoSegundos, valorMissao)
              item('missao 10',         52,     10),
              item('missao 11',       11,     70),
              item('missao 12',       32,     30)],
-        length(L, N),
-        length(R, N),
-        R ins 0..1,
-        maplist(arg(2), L, LW),
-        maplist(arg(3), L, LV),
-        scalar_product(LW, R, #=<, 150),
-        scalar_product(LV, R, #=, VM),
-        labeling([max(VM)], R),
-        scalar_product(LW, R, #=, WM),
+        length(L, N), %% L - lista de itens   N - tamanho da lista de itens
+        length(R, N), %% R - lista de tamanho igual a L   N - tamanho da lista
+
+        R ins 0..1, %% R faz parte do domínio 0..1
+
+        maplist(arg(2), L, LW), %% :Goal, List1, List2 - opera com dois elementos da lista    Lw - lista de peso
+        maplist(arg(3), L, LV), %% LV - lista de valor
+
+        scalar_product(LW, R, #=<, 400), %% (Cs, Vs, Rel, Expr) cs é uma lista de inteiros e vs uma lista de variaveis e inteiros
+        scalar_product(LV, R, #=, VM), %% #=< - X(cs) é menor ou igual a Y(vs)     #= - X é igual Y 
+
+        labeling([max(VM)], R), %% (options, vars)
+
+        scalar_product(LW, R, #=, WM), %% WM = peso máximo
+
         %% affichage des résultats
-        compute_lenword(L, 0, Len),
+        compute_lenword(L, 0, Len), %% compara o nome do item com 0 e mostra qual tem o tamanho maior
         sformat(A1, '~~w~~t~~~w|', [Len]),
         sformat(A2, '~~t~~w~~~w|', [4]),
         sformat(A3, '~~t~~w~~~w|', [5]),
         print_results(A1,A2,A3, L, R, WM, VM).
- 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % to show the results in a good way
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
