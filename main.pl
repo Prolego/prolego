44c426ea09ce505281df57ffc9b6f2d6d06b73d2
Carolina Ramalho <carol15022@hotmail.com>
2016-06-03 15:57:40 -0300

    Integração com Traveller

--- main.pl
+++ main.pl
@@ -3,14 +3,21 @@ prolego:-
   consult('prolego.pl'),
   conjunto(Missoes),
   algoritmo01(Missoes, 50, Subconjunto),
-  resultado(Subconjunto).
+  open('resultado.pl',write,Stream),  
+  write(Stream, 'teste(A):- A = "'),
+  resultado(Subconjunto, Stream),
+  write(Stream, '"'),
+  close(Stream),
+  consult('resultado.pl'),
+  teste(A),
+  string_concat("cmd.exe /C java -jar teste.jar ", A, Result),
+  shell(Result).
   
-resultado([]).	  
-resultado([X|Y]):-
+resultado([], _).	  
+resultado([X|Y], Stream):-
 	consult('conjunto.pl'),
-	PontoInicial = "0, 0", 
+	PontoInicial = "0 0", 
 	pontos(X, Final),
-	write(PontoInicial),
-	write(", "),
-	write(Final),
-	resultado(Y).
+	write(Stream, PontoInicial),
+	write(Stream, Final),
+	resultado(Y, Stream).
