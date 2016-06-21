d639b3366e13e3cd31b7a903d298b31bc5788ec0
Carolina Ramalho <carol15022@hotmail.com>
2016-06-20 23:20:05 -0300

    integração com Traveller

--- main.pl
+++ main.pl
@@ -1,23 +1,13 @@
-prolego:-
-  consult('conjunto.pl'),
-  consult('prolego.pl'),
-  conjunto(Missoes),
-  algoritmo01(Missoes, 50, Subconjunto),
-  open('resultado.pl',write,Stream),  
-  write(Stream, 'teste(A):- A = "'),
-  resultado(Subconjunto, Stream),
-  write(Stream, '"'),
+prolego(NumeroConjunto):-
+  consult('conjuntoMissoes.pl'),
+  consult('algoritmoDeDecisao.pl'),
+  conjunto(NumeroConjunto, Missoes),
+  algoritmo01(Missoes, 150, Subconjunto),
+  open('stringDePontos.pl',write,Stream),  
+  write(Stream, 'stringDePontos(Pontos):- Pontos = "'),
+  consult('escritaPontosEmArquivo.pl'),
+  escritaPontosEmArquivo(Subconjunto, Stream),
+  write(Stream, '".'),
   close(Stream),
-  consult('resultado.pl'),
-  teste(A),
-  string_concat("cmd.exe /C java -jar teste.jar ", A, Result),
-  shell(Result).
-  
-resultado([], _).	  
-resultado([X|Y], Stream):-
-	consult('conjunto.pl'),
-	PontoInicial = "0 0", 
-	pontos(X, Final),
-	write(Stream, PontoInicial),
-	write(Stream, Final),
-	resultado(Y, Stream).
+  consult('integracaoComTraveller.pl').
+  /*integracaoComTraveller.*/
